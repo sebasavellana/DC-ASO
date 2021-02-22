d508aa763457b7abdfe02349aa3ee67e765698f1
sebasavellana <35118660+sebasavellana@users.noreply.github.com>
2021-02-22 11:34:26 +0100

    Update cleanconfmasiva.ps1
    Uso de excepciones personalizadas

--- cleanconfmasiva.ps1
+++ cleanconfmasiva.ps1
@@ -1,13 +1,31 @@
+$ErrorActionPreference = "Stop"
+
 $deptfile = Import-Csv "C:\Users\Administrador\Documents\Departamentos.csv"
 
 # ¿Qué se tiene que ejecutar una vez?
 # ¿Qué se tiene que ejecutar para cada línea del fichero?
 
 foreach ($line in $deptfile){
+    $departamento = $line.NombreDepartamento
     # 1.1 - Eliminar las comparticiones de SMB
-    Remove-SmbShare -Name ("Departamento" + $line.NombreDepartamento) -Force
+    try{
+        Remove-SmbShare -Name ("Departamento" + $departamento) -Force -ErrorAction Stop
+    }
+    catch [Microsoft.PowerShell.Cmdletization.Cim.CimJobException]{
+        Write-Output ("Compartición de " + $departamento + " ya eliminada")
+    }
     # 1.2 - Eliminar grupos de AD
-    Remove-ADGroup -Identity $line.NombreDepartamento -Confirm:$false
+    try{
+        Remove-ADGroup -Identity $departamento -Confirm:$false
+    }
+    catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]{
+        Write-Output ("Grupo de AD " + $departamento + " ya eliminado")
+    }
     # 1.3 - Eliminar el directorio compartido
-    Remove-Item -Path ("C:\Compartida\" + $line.NombreDepartamento)
+    try {
+        Remove-Item -Path ("C:\Compartida\" + $departamento)
+    }
+    catch [System.Management.Automation.ItemNotFoundException] {
+        Write-Output "Directorio compartido de departamento ya eliminado"
+    }
 }
