5cc23a424762896f485ec6f4dc0b769ff3c1dd8f
sebasavellana <35118660+sebasavellana@users.noreply.github.com>
2021-02-28 12:52:13 +0100

    Create confmasiva.py

---
+++ confmasiva.py
@@ -0,0 +1,28 @@
+import pandas
+import subprocess, sys
+
+try:
+    dfusers = pandas.read_csv("/root/pandas/usuariossamba.csv")
+    dfdepts = pandas.read_csv("/root/pandas/departamentos.csv")
+except:
+    print("Fichero de datos no encontrado")
+    sys.exit()
+
+depts = dfdepts.values.tolist()
+
+# Creación de grupos departamentales
+for item in depts:
+    subprocess.run(["samba-tool","group","add",item[0]])
+
+
+users = dfusers.values.tolist()
+# 5 es username, 6 es password, 8 es el departamento al que pertenecen
+
+# Creación de usuarios, activación y adición al grupo del departamento
+# Además, informática serán Administradores del Dominio
+for item in users:     
+    subprocess.run(["samba-tool","user","create",item[5],item[6]])
+    subprocess.run(["samba-tool","user","enable",item[5]])
+    subprocess.run(["samba-tool","group","addmembers",item[8],item[5]]) 
+    if item[8] == "Informatica":
+        subprocess.run(["samba-tool","group","addmembers","Domain Admins",item[5]])
